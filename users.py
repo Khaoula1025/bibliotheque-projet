fc0bd0c2d8cdcb02a6d2de0d9d2bf3b20cde064f
Khaoula Esioudi <khaoula.esioudi@gmail.com>
2025-09-26 11:58:07 +0100

    added assosiationUtilisateursLivre

--- users.py
+++ users.py
@@ -6,3 +6,9 @@ def utilisateursMajeurs():
 def nomCompletMaj():
    return list(map(lambda x:x[2].upper(),d.utilisateurs))
 
+def assosiationUtilisateursLivre():
+   utilisateur_livre={}
+   for i in list(zip(d.utilisateurs,d.livres)) :
+      utilisateur_livre[i[0][1]]=i[1]['titre']
+   print(utilisateur_livre)
+   
