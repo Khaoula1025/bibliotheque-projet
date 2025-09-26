0018fb85b044258dce77a67fc372cb456da17bad
Khaoula Esioudi <khaoula.esioudi@gmail.com>
2025-09-26 11:57:37 +0100

    added uppercase username

--- users.py
+++ users.py
@@ -2,3 +2,7 @@ import data as d
 
 def utilisateursMajeurs():
    return list(filter(lambda x:x[3]>=18,d.utilisateurs))
+
+def nomCompletMaj():
+   return list(map(lambda x:x[2].upper(),d.utilisateurs))
+
