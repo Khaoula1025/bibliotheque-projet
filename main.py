6fd106fda78bb3960908cad1beb5790f93949948
Khaoula Esioudi <khaoula.esioudi@gmail.com>
2025-09-26 15:58:32 +0100

    added functions to main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import users 
-
-#print('les utilisateurs majeurs sont : ',users.utilisateursMajeurs())
-#print('les utilisateurs des utilisateurs en majuscule: ',users.nomCompletMaj())
-users.assosiationUtilisateursLivre()
+from data import utilisateurs
+from data import aime_livres
+print('les utilisateurs majeurs sont : ',users.utilisateursMajeurs(utilisateurs))
+print('les utilisateurs des utilisateurs en majuscule: ',users.nomCompletMaj(utilisateurs))
+print(users.assosiationUtilisateursLivre(utilisateurs,aime_livres))
+print(users.user_livre(utilisateurs,aime_livres))
--- users.py
+++ users.py
@@ -1,15 +1,15 @@
 import data as d 
 
-def utilisateursMajeurs():
-   return list(filter(lambda x:x[3]>=18,d.utilisateurs))
+def utilisateursMajeurs(utilisateurs):
+   return list(filter(lambda x:x[3]>=18,utilisateurs))
 
-def nomCompletMaj():
-   return list(map(lambda x:x[2].upper(),d.utilisateurs))
+def nomCompletMaj(utilisateurs):
+   return list(map(lambda x:x[2].upper(),utilisateurs))
 
-def assosiationUtilisateursLivre():
+def assosiationUtilisateursLivre(utilisateurs,aime_livres):
    utilisateur_livre={}
-   for i in d.utilisateurs:
-     for j in d.aime_livres:
+   for i in utilisateurs:
+     for j in aime_livres:
        if i[0]==j[0]:
              if i[1] not in utilisateur_livre:
               utilisateur_livre[i[1]]=[]
@@ -20,11 +20,6 @@ def associateNamesWithFbooks(x):
        for i in d.utilisateurs:
         if i[1]==x[0]:
            return f'{i[1]} {i[2]} ({i[3]}ans ) aime {x[1]}'
-def user_livre():
- 
-           
-    #user_livre=list(map(lambda x:f'{i[1]} {i[2]} ({i[3]}ans ) aime {x[1]}'if x[0]==i[1] else '',assosiationUtilisateursLivre().items()))
-      return list(map(associateNamesWithFbooks,assosiationUtilisateursLivre().items()))
-        
-print(user_livre())
-#print(assosiationUtilisateursLivre().items())
+def user_livre(utilisateurs,aime_livres):
+       return list(map(associateNamesWithFbooks,assosiationUtilisateursLivre(utilisateurs,aime_livres).items()))
+
