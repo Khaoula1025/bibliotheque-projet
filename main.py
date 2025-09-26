c435caff7c0d09fb17f88bd2ae58434b94ab48a9
Khaoula Esioudi <khaoula.esioudi@gmail.com>
2025-09-26 15:42:44 +0100

    added user_livre

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import users
 
 #print('les utilisateurs majeurs sont : ',users.utilisateursMajeurs())
 #print('les utilisateurs des utilisateurs en majuscule: ',users.nomCompletMaj())
-print(users.assosiationUtilisateursLivre())
+users.assosiationUtilisateursLivre()
--- users.py
+++ users.py
@@ -8,7 +8,23 @@ def nomCompletMaj():
 
 def assosiationUtilisateursLivre():
    utilisateur_livre={}
-   for i in list(zip(d.utilisateurs,d.livres)) :
-      utilisateur_livre[i[0][1]]=i[1]['titre']
-   print(utilisateur_livre)
-   
+   for i in d.utilisateurs:
+     for j in d.aime_livres:
+       if i[0]==j[0]:
+             if i[1] not in utilisateur_livre:
+              utilisateur_livre[i[1]]=[]
+             utilisateur_livre[i[1]].append(j[1])
+          
+   return utilisateur_livre
+def associateNamesWithFbooks(x):
+       for i in d.utilisateurs:
+        if i[1]==x[0]:
+           return f'{i[1]} {i[2]} ({i[3]}ans ) aime {x[1]}'
+def user_livre():
+ 
+           
+    #user_livre=list(map(lambda x:f'{i[1]} {i[2]} ({i[3]}ans ) aime {x[1]}'if x[0]==i[1] else '',assosiationUtilisateursLivre().items()))
+      return list(map(associateNamesWithFbooks,assosiationUtilisateursLivre().items()))
+        
+print(user_livre())
+#print(assosiationUtilisateursLivre().items())
