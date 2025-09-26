17ac82535d92a02dc703b83b0b5f35e3cd15a74d
Khaoula Esioudi <khaoula.esioudi@gmail.com>
2025-09-26 16:32:59 +0100

    made changes in main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import users 
 from data import utilisateurs
 from data import aime_livres
-print('les utilisateurs majeurs sont : ',users.utilisateursMajeurs(utilisateurs))
-print('les utilisateurs des utilisateurs en majuscule: ',users.nomCompletMaj(utilisateurs))
+# print('les utilisateurs majeurs sont : ',users.utilisateursMajeurs(utilisateurs))
+# print('les utilisateurs des utilisateurs en majuscule: ',users.nomCompletMaj(utilisateurs))
 print(users.assosiationUtilisateursLivre(utilisateurs,aime_livres))
-print(users.user_livre(utilisateurs,aime_livres))
+#print(users.user_livre(utilisateurs,aime_livres))
