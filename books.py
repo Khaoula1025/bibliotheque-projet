d9822c7db1e65684e7a431a22e4b936424b35251
SaidaAourras <sa123aouras@gmail.com>
2025-09-26 12:26:17 +0100

    complter la logique du fonction show_2_by_2 et le main

--- books.py
+++ books.py
@@ -2,20 +2,24 @@ import data
 
 livres = data.livres
 
+#q1
 def sort_func(x):
     myList = list(x.items())
     return myList[1]
    
     
-def traige_livre(livres):
+def triage_livre(livres):
     livres.sort(key=sort_func)
     return livres
 
+#q2
 def plus_ancien(livres):
-    return (traige_livre(livres))[0]
+    return (triage_livre(livres))[0]
 
 def plus_recent(livres):
-    return (traige_livre(livres))[-1]
+    return (triage_livre(livres))[-1]
+
+#q3
 
 new_dict ={ "1984": 2, "Le Petit Prince": 1, "Harry Potter": 1 } 
 
@@ -23,7 +27,6 @@ def addNewBook(livres):
     livres.append(new_dict)
     return livres
 
-# print( addNewBook(livres))
 
 def show_2_by_2(livres):
     choix = '>'
@@ -33,17 +36,20 @@ def show_2_by_2(livres):
             print(f'livre de {debut} a {fin}\n')
             for i in range(debut , fin):
                 
-                print((traige_livre(livres))[i])
+                print((triage_livre(livres))[i])
             print('\ntaper \'>\' pour passer au deux livre suivant ')
             print('taper autre chose pour quitter ')
             choix = input('votre choix : ')
             if choix.strip() == '>':
                 debut +=2
-                fin +=2 
-                if fin > len(traige_livre(livres)):
+                fin +=1
+                if fin > len(triage_livre(livres)):
                     print('--------- la fin du liste -----------')
                     break
+                elif fin < len(triage_livre(livres)):
+                    fin +=1
+                else:
+                    pass
             else:
                 break
             
-show_2_by_2(livres)
---
+++ main.py
@@ -0,0 +1,6 @@
+import data
+import books
+
+livres = data.livres
+
+books.show_2_by_2(livres)
